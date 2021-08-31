39b2a6953cea3103d49def9a50a4bc9fc725e629
Kavya Pandey <57443445+kavya-pandey@users.noreply.github.com>
2021-08-31 09:41:42 +0530

    a quiz application using python

---
+++ QUESTION3/quizApp.py
@@ -0,0 +1,146 @@
+from tkinter import *
+from tkinter import messagebox as mb
+import json
+
+class Quiz:
+	
+	def __init__(self):
+		
+		self.q_no=0
+		
+		self.display_title()
+		self.display_question()
+		
+		
+		self.opt_selected=IntVar()
+		
+		
+		self.opts=self.radio_buttons()
+		
+		self.display_options()
+		
+		self.buttons()
+		
+		self.data_size=len(question)
+		
+		self.correct=0
+
+
+	
+	def display_result(self):
+		
+		wrongAns = self.data_size - self.correct
+		correctAns = f"Correct: {self.correct}"
+		wrong = f"Wrong: {wrongAns}"
+		
+		score = int(self.correct / self.data_size * 100)
+		result = f"Score: {score}%"
+		
+		mb.showinfo("Result", f"{result}\n{correctAns}\n{wrong}")
+
+
+	def check_ans(self, q_no):
+		
+		if self.opt_selected.get() == answer[q_no]:
+			return True
+
+	
+	def next_btn(self):
+		
+		if self.check_ans(self.q_no):
+			
+			self.correct += 1
+		
+		self.q_no += 1
+		
+		if self.q_no==self.data_size:
+			
+			self.display_result()
+			
+			gui.destroy()
+		else:
+			self.display_question()
+			self.display_options()
+
+
+
+	def buttons(self):
+		
+		
+		next_button = Button(gui, text="Next",command=self.next_btn,
+		width=10,bg="Green",fg="Black",font=("ariel",16,"bold"))
+		
+		next_button.place(x=300,y=380)
+		
+		quit_button = Button(gui, text="END", command=gui.destroy,
+		width=5,bg="RED", fg="BLACK",font=("ariel",16," bold"))
+		
+		quit_button.place(x=700,y=380)
+
+
+
+	def display_options(self):
+		val=0
+		
+		self.opt_selected.set(0)
+		
+		
+		for option in options[self.q_no]:
+			self.opts[val]['text']=option
+			val+=1
+
+
+	def display_question(self):
+		
+		q_no = Label(gui, text=question[self.q_no], width=60,
+		font=( 'ariel' ,16, 'bold' ), anchor= 'w' )
+		
+		q_no.place(x=70, y=100)
+
+
+	def display_title(self):
+		
+		title = Label(gui, text="Python Quiz",
+		width=50, bg="Yellow",fg="Blue", font=("ariel", 20, "bold"))
+		
+		title.place(x=0, y=2)
+
+
+	
+	def radio_buttons(self):
+		
+		ques_list = []
+		
+		y_pos = 150
+		
+		while len(ques_list) < 4:
+			
+			radio_btn = Radiobutton(gui,text=" ",variable=self.opt_selected,
+			value = len(ques_list)+1,font = ("ariel",14))
+			
+			ques_list.append(radio_btn)
+			
+			radio_btn.place(x = 100, y = y_pos)
+			
+			y_pos += 40
+		
+		return ques_list
+
+gui = Tk()
+
+gui.geometry("800x450")
+
+gui.title("Python Quiz")
+
+with open("C:/Users/KAVYA/Desktop/7th sem/Assignment_1/QUESTION3/data.json") as f:
+	data = json.load(f)
+
+question = (data['question'])
+options = (data['options'])
+answer = (data[ 'answer'])
+
+quiz = Quiz()
+
+gui.mainloop()
+
+
